05d4cbdd5ea4b0374fb85d45290c3a8c413012fa
ngotantinh96 <ngotantinh96@gmail.com>
2024-04-12 17:21:00 +0700

    feat: Global Error Handling with Middleware, Exception Filter Attribute, Problem Details, Error Endpoint, Custom Problem Details Factory

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,16 +1,32 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+    // builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+    // app.Map("/error", (HttpContext httpContext) => {
+    //     var exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+    //     return Results.Problem(extensions: new Dictionary<string, object> {
+    //         { "CustomProperty", "CustomValue" }
+    //     });
+    // });
+
     app.UseHttpsRedirection();
     app.MapControllers();
 
