9296b3e42e11b814858b287e3400f39fb7712698
ngotantinh96 <ngotantinh96@gmail.com>
2024-04-25 13:29:29 +0700

    feat: Implement AggregateRoot, Entity, ValueObject

---
+++ BuberDinner.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,9 @@
+namespace BuberDinner.Domain.Models;
+
+public class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+}
---
+++ BuberDinner.Domain/Models/ValueObject.cs
@@ -0,0 +1,51 @@
+namespace BuberDinner.Domain.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj is not ValueObject)
+            return false;
+
+        var valueObject = (ValueObject)obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !Equals(left, right);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?)other);
+    }
+}
+
+public class Price(decimal amount, string currency) : ValueObject
+{
+    public decimal Amount { get; private set; } = amount;
+    public string Currency { get; private set; } = currency;
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Amount;
+        yield return Currency;
+    }
+}
