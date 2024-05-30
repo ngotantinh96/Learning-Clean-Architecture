f9c8c8ec195a9f9e54507943c660fd52ba539cdd
ngotantinh96 <ngotantinh96@gmail.com>
2024-05-30 16:37:38 +0700

    feat: implement strongly-typed id

--- BuberDinner.Domain/Common/Models/AggregateRoot.cs
+++ BuberDinner.Domain/Common/Models/AggregateRoot.cs
@@ -1,13 +1,18 @@
 namespace BuberDinner.Domain.Common.Models;
 
-public class AggregateRoot<TId> : Entity<TId>
-    where TId : notnull
+public class AggregateRoot<TId, TIdType> : Entity<TId>
+    where TId : AggregateRootId<TIdType>
 {
-    protected AggregateRoot(TId id) : base(id)
+    public new AggregateRootId<TIdType> Id { get; protected set; }
+
+    protected AggregateRoot(TId id)
     {
+        Id = id;
     }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     protected AggregateRoot()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
     {
     }
 }
